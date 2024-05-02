002566ec73bbbba0c31d7809cec405b074b03e6e
YoungHaChoi <askillofgod159@naver.com>
2024-05-02 16:58:15 +0900

    dockerfile add, ci/cd

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound, TranscriptsDisabled
+from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound, TranscriptsDisabled, VideoUnavailable
 
 app = Flask(__name__)
 
@@ -19,12 +19,13 @@ def get_subtitle():
         refined_transcript = [entry['text'] for entry in transcript]
         return jsonify(refined_transcript)
     except NoTranscriptFound:
-        return jsonify({"error": "해당 동영상은 자막을 지원하지 않거나 한국영상이 아닙니다."}), 404
-    except TranscriptsDisabled:
-        return jsonify({"error": "해당 동영상에서는 자막 사용이 비활성화되어 있습니다."}), 403
+        return jsonify({"error": "자막을 찾을 수 없습니다. 비디오가 자막을 지원하지 않거나 요청된 언어가 지원되지 않을 수 있습니다."}), 404
+    except VideoUnavailable:
+        return jsonify({"error": "비디오 ID가 유효하지 않거나 비디오가 사용할 수 없습니다. 비디오 ID를 확인하세요."}), 404
+    except ConnectionError:
+        return jsonify({"error": "YouTube에 연결할 수 없습니다. 네트워크 연결을 확인하고 다시 시도하세요."}), 503
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
+        return jsonify({"error": "알 수 없는 오류가 발생했습니다: " + str(e)}), 500
 
 
 
